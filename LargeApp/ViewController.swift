648b3088418539ded04d0b036adc48ff183714a0
CostOfSociety <48133616+CostOfSociety@users.noreply.github.com>
2022-10-01 19:52:02 +0300

    Initial Commit

---
+++ LargeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LargeApp
+//
+//  Created by Святослав on 01.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
